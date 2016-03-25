6e028c1633489f0aeeb3248f838ce0d248f478d6
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2016-03-25 18:03:32 +1100

    Add more unit test in Momorisation class

--- src/test/java/org/paradise/function/MomorisationTest.java
+++ src/test/java/org/paradise/function/MomorisationTest.java
@@ -31,6 +31,12 @@ public class MomorisationTest {
 
         System.out.println("Second call");
         assertEquals("Incorrect memorised double return", Double.valueOf(4.0), memorisation.memorisedDoubler.apply(2.0));
+
+        System.out.println("Third call");
+        assertEquals("Incorrect memorised double return", Double.valueOf(6.0), memorisation.memorisedDoubler.apply(3.0));
+
+        System.out.println("Fourth call");
+        assertEquals("Incorrect memorised double return", Double.valueOf(6.0), memorisation.memorisedDoubler.apply(3.0));
     }
 
 }
